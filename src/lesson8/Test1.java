82740dad961f2ff99ffd4dcabe8c02100db3141e
Artem <Artem@host.docker.internal>
2023-08-22 16:29:05 +0700

    7 урок

---
+++ src/lesson8/Test1.java
@@ -0,0 +1,17 @@
+
+package lesson8;
+
+public class Test1 {
+    
+    public final int a=10;
+    
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        t.a= t.a*2;
+        System.out.println(t.a);
+        
+        
+        
+    }
+    
+}
