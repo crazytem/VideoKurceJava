b9f786f863a875a2dc0e3c094d96fe80062e2503
Artem <Artem@host.docker.internal>
2023-08-25 15:50:45 +0700

    lesson 9

---
+++ src/lesson9/Student.java
@@ -0,0 +1,13 @@
+
+package lesson9;
+
+public class Student {
+    
+}
+class StudentTest{
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3;
+    }
+}
